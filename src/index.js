315ce55108acfcef3137b569f46e82b5fa873d22
Kopochev <copochev@yandex.ru>
2020-03-05 19:17:34 +0300

    feat: task is solved

--- src/index.js
+++ src/index.js
@@ -36,9 +36,72 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
+function translateToNumber(morse){
+    let numberMorse = {};
+    let arrKeys = Object.keys(morse);
 
+    //primary state numberMorse
+    for(let init = 0; init < arrKeys.length; init++){
+        numberMorse[arrKeys[init]] = '';
+    }
+    // add value in form numbers
+    for(let i = 0; i < arrKeys.length; i++){
+        for(let j = 0 ; j<arrKeys[i].length; j++){
+            if(arrKeys[i][j] === '.'){
+                numberMorse[arrKeys[i]] += '10'; 
+            }
+            else {
+                numberMorse[arrKeys[i]] += '11';
+            }
+        }
+    }
+    //add zero numbers
+    for(let key in numberMorse){
+        let zeroStart = '0';
+        let zero = '';
+        
+        if(numberMorse[key].length < 10){
+            let counter = 10 - numberMorse[key].length;
+            for(let i = 0; i < counter; i++){
+                zero += zeroStart;
+            }
+            let splitMorse = numberMorse[key].split('');
+            splitMorse.splice(0,0,zero);
+            let finalCode = splitMorse.join('');
+            numberMorse[key] = finalCode;
+        }
+    }
+    return numberMorse;
+}
 function decode(expr) {
-    // write your solution here
+   let morse = translateToNumber(MORSE_TABLE);
+    //divide encoded message on 10
+    let arr = [];
+    let count = 0;
+    let strExpr = '';
+    for (let i = 0; i < expr.length; i++) {
+        count++;
+        strExpr += expr[i]
+        if (count == 10) {
+            arr.push(strExpr);
+            count = 0;
+            strExpr = '';
+        }
+    }
+        //finish decode message
+        let finishDecode = '';
+        for (let i = 0; i < arr.length; i++) {
+            for (let key in morse) {
+                if (morse[key] == arr[i]) {
+                    finishDecode += MORSE_TABLE[key];
+                }
+    
+            }
+            if (arr[i] == '**********') {
+                finishDecode += ' ';
+            }
+        }
+        return finishDecode;
 }
 
 module.exports = {
